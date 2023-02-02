0b308e2b17f2313a39eb68b522778bce88e3b7d9
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-02-02 22:19:42 +0530

    updated

---
+++ js/sweet.js
@@ -0,0 +1,6 @@
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script type="text/javascript">
+function sweetalert() {
+			swal.fire('it can work like this also');
+}
+</script>
